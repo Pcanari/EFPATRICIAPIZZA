c59be093de38b0a1f9944e733fc3daca3c62cb8f
Pcanari <patriciacanarihuaman@gmail.com>
2022-08-17 03:36:07 -0500

    pro

---
+++ src/main/java/EFPATRICIACAÑARI/Service/PizzeriaService.java
@@ -0,0 +1,20 @@
+package EFPATRICIACAÑARI.Service;
+
+import java.util.List;
+
+import EFPATRICIACAÑARI.Model.Pizzeria;
+
+
+
+public interface PizzeriaService {
+	
+
+	List<Pizzeria> listar();
+	Pizzeria ObtenerId(Integer id);	
+	void guardar(Pizzeria pizzeria);
+	void eliminar(Integer id);
+	void actualizar(Pizzeria pizzeria);
+	
+	void asignarPizzaAPizzeria();
+
+}
